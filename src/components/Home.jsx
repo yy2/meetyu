22abacfd35edd43657a405088ea74350ccfd9da2
Yu <yuzyuan93@gmail.com>
2020-02-23 18:58:57 -0500

    Removed button component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import pic from "../images/profile.png";
 import "../css/Home.css";
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 
